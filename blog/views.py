4d64f0180a0c84e19158811059096e790d1da740
xiaoyue8 <xiaoyue8@outlook.com>
2018-12-14 14:02:53 +0800

    1.djangos视图，新增页面；2.gitignore配置

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,23 @@
 # Create your views here.
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
+
+#def index(req):
+#    return HttpResponse('<h1>hello welcome to Django</h1>')
+
+
+from django.shortcuts import render_to_response
+class Person(object):
+    def __init__(self,name,age,sex):
+	self.name = name
+	self.age = age
+	self.sex = sex
+
+    def say(self):
+	return "I'm"+self.name
 
 def index(req):
-    return HttpResponse('<h1>hello welcome to Django</h1>')
+    user = {'name':'tom','age':23,'sex':'male'}
+    book_list = ['python','java','php','web']
+
+    return render_to_response('index.html',{'title':'my page','book_list':book_list,'user':user})
