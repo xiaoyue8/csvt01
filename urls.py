4db8ab7642b7c92e1ed826c4b0ad5921787dd12e
xiaoyue8 <xiaoyue8@outlook.com>
2018-12-14 16:05:04 +0800

    2.Djangos视图和URL配置：2.url配置方式2

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from blog.views import index
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,5 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/index/$','blog.views.index'),	
-)
+    # url(r'^blog/index/$','blog.views.index'),
+    url(r'^blog/index/$',index),
+)	
